65f73151266025588b370d504c4ca12d785501c7
haggar39 <hisseinbaharfadoul@email.com>
2025-10-15 00:34:34 +0000

    test automatisé

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -6,5 +6,25 @@ def is_multiple_of_3_or_5?(number) # rubocop:disable Naming/PredicatePrefix
   false
 end
 
-def sum_of_3_or_5_multiples(final_number)
-end
+ def sum_of_3_or_5_multiples(final_number)
+
+     final_sum = 0 
+      
+      
+     (0...final_number).each do |number|
+
+
+     
+     if is_multiple_of_3_or_5?(number) 
+
+      final_sum += number
+     
+
+        
+     end
+
+    end
+     return final_sum 
+    
+   end
+
--- spec/multiples_spec.rb
+++ spec/multiples_spec.rb
@@ -23,3 +23,17 @@ describe 'the is_multiple_of_3_or_5? method' do
     expect(is_multiple_of_3_or_5?(22)).to eq(false)
   end
 end
+
+describe 'the sum of multiple of 3 or 5? method' do 
+  it 'should return the result' do
+    expect(sum_of_3_or_5_multiples(10)).to eq(23)
+    expect(sum_of_3_or_5_multiples(11)).to eq(33)
+  end
+
+# Aichetou le code a fonctionner
+
+  it 'should refuses the entry of the user' do
+    expect(sum_of_3_or_5_multiples(-1)).to eq(0)
+    expect(sum_of_3_or_5_multiples(11)).to eq(33)
+  end
+end
